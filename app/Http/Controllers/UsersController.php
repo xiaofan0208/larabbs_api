1389d5e1560b96ec607a41b2bdf67dd6f9688b4b
xiaofan0208 <xiaofan0208@126.com>
2018-08-01 13:13:40 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,14 @@ use App\Handlers\ImageUploadHandler ;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        /*  设定 指定动作 不使用 Auth 中间件进行过滤
+            除了此处指定的动作以外，所有其他动作都必须登录用户才能访问
+        */
+        $this->middleware('auth',['except' => ['show'] ]);
+    }
+
     //显示用户个人信息页面
     public function show(User $user)
     {
@@ -17,11 +25,13 @@ class UsersController extends Controller
     //显示编辑个人资料页面
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
     //处理 edit 页面提交的更改
     public function update(UserRequest $request , ImageUploadHandler $uploader ,  User $user )
     {
+        $this->authorize('update',$user);
         $data = $request->all();
         if( $request->avatar ){
             $result =  $uploader->save( $request->avatar  , 'avatars' , $user->id , 362) ;
