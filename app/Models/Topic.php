5aa1ca8fd31fc7e5f4d219e8d00d569343420798
xiaofan0208 <xiaofan0208@126.com>
2018-08-02 14:21:19 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,18 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //获取到话题对应的分类
+    public function category()
+    {
+        //一个话题属于一个分类
+        return $this->belongsTo(Category::class);
+    }
+    //话题对应的作者
+    public function user()
+    {
+        //一个话题拥有一个作者。
+        return $this->belongsTo(User::class);
+    }
+
 }
