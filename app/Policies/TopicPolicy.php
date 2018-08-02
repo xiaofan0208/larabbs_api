0e920b42c818a6d2faa5021b365b3db49c9637ac
xiaofan0208 <xiaofan0208@126.com>
2018-08-02 13:05:42 +0000

    Seeding topics and users data

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -5,6 +5,7 @@ namespace App\Policies;
 use App\Models\User;
 use App\Models\Topic;
 
+
 class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use Carbon\Carbon ;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -14,10 +14,18 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+    static $password;
+    //Carbon 是 PHP DateTime 的一个简单扩展，这里我们使用 now() 和 toDateTimeString() 来创建格式如 2017-10-13 18:42:40 的时间戳
+    $now = Carbon::now()->toDateTimeString();
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+     //   'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'password' => $password ?: $password = bcrypt('password'),
+        'introduction' => $faker->sentence(),
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
