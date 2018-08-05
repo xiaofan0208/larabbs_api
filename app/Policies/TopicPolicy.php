e1d32288dacb0ad1c3dac122b569d512903f6d28
xiaofan0208 <xiaofan0208@126.com>
2018-08-05 09:58:56 +0000

    Users can edit their own topic

--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -8,10 +8,10 @@ use App\Models\Topic;
 
 class TopicPolicy extends Policy
 {
+    // 编辑文章时，只有当话题关联作者的 ID 等于当前登录用户 ID 时候才放行
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        return $topic->user_id == $user->id;
     }
 
     public function destroy(User $user, Topic $topic)
