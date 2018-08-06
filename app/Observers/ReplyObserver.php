6ffea566ed08a2d009389a02868a83ef6a048528
xiaofan0208 <xiaofan0208@126.com>
2018-08-06 15:03:49 +0000

    User can create reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,15 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
+    // 数据成功创建时，created 方法将会被调用。
+    public function created(Reply $reply)
+    {
+        $reply->topic->increment('reply_count',1) ;
+    }
+    // 对 content 字段进行净化处理
     public function creating(Reply $reply)
     {
-        //
+        $reply->content = clean( $reply->content,'user_topic_body' );
     }
 
-    public function updating(Reply $reply)
-    {
-        //
-    }
 }
