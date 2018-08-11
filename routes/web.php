c823fc4888f99d7773a6c23e1e9fcf084f479124
xiaofan0208 <xiaofan0208@126.com>
2018-08-11 11:15:32 +0000

    Admin permissions

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,6 @@ Route::get('topics/{topic}/{slug?}' , 'TopicsController@show' )->name('topics.sh
 Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
 Route::resource('notifications' ,'NotificationsController' , ['only' => ['index'] ] );
+
+Route::get('permission-denied' ,'PagesController@permissionDenied' )->name('permission-denied');
+
