45b32568c149c6a60d3cb6b4a2e7b7797012eaef
xiaofan0208 <xiaofan0208@126.com>
2018-08-11 06:55:59 +0000

    Admin can manage site settings

--- config/administrator.php
+++ config/administrator.php
@@ -89,6 +89,9 @@ return array(
             'topics',
             'replies',
         ],
+        '站点管理' => [
+            'settings.site',
+        ],
     ],
 
     /*
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" >
      <!-- CSRF Token -->
     <meta  name="csrf-token" content="{{ csrf_token() }}" >
-    <title>@yield('title','LaraBBS')- Laravel 进阶教程</title>
+    <title>@yield('title','LaraBBS')- {{ setting('site_name' , 'Laravel 进阶教程') }}</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- <link rel="stylesheet" href="/css/app.css"> -->
     @yield('styles')
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶</title>
-    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
+    <meta name="description" content="@yield('description', setting('seo_description', 'LaraBBS 爱好者社区。'))" />
+    <meta name="keyword" content="@yield('keyword', setting('seo_keyword', 'LaraBBS,社区,论坛,开发者论坛'))" />
+
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page" >
