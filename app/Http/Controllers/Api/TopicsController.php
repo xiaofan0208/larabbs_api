4c34ddb52603e4e539e55fdc8ac83a58c72700b7
xiaofan0208 <xiaofan0208@126.com>
2018-08-25 15:08:14 +0800

    topic index

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,6 +6,7 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\TopicRequest;
 use App\Transformers\TopicTransformer;
+use App\Models\User;
 
 class TopicsController extends Controller
 {
@@ -35,5 +36,39 @@ class TopicsController extends Controller
 
         $topic->delete();
         return $this->response->noContent();
+    }
+
+    // 话题列表
+    public function index(Request $request,Topic $topic)
+    {
+      //  临时关闭 DingoApi 的预加载 
+      //  app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+
+        $query = $topic->query();
+        
+        if( $categoryId = $request->category_id ){
+            $query->where('category_id', $categoryId);
+        }
+
+        // 为了说明 N+1问题，不使用 scopeWithOrder
+        switch($request->order ){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        $topics = $query->paginate(20);
+        return $this->response->paginator($topics , new TopicTransformer() );
+    }
+
+    //某个用户的发布的话题
+    public function userIndex(Request $request , User $user)
+    {
+        $topics = $user->topics()->recent()->paginate(20) ;
+
+        return $this->response->paginator($topics,new TopicTransformer());
     }
 }
--- app/Transformers/TopicTransformer.php
+++ app/Transformers/TopicTransformer.php
@@ -8,6 +8,12 @@ use League\Fractal\TransformerAbstract;
 // 话题数组
 class TopicTransformer  extends TransformerAbstract
 {
+    /*  额外可以获取的资源
+        availableIncludes 中的每一个参数都对应一个具体的方法，
+        方法命名规则为 include + user 、 include + category 驼峰命名。
+    */
+    protected $availableIncludes = ['user', 'category'];
+
     public function transform(Topic $topic)
     {
         return [
@@ -24,5 +30,16 @@ class TopicTransformer  extends TransformerAbstract
             'created_at' => $topic->created_at->toDateTimeString(),
             'updated_at' => $topic->updated_at->toDateTimeString(),
         ];
+    }
+
+    // 如果在请求中包含 include=user，则TopicTransformer 自动调用 includeUser 方法。
+    public function includeUser(Topic $topic )
+    {
+        return $this->item($topic->user , new UserTransformer() );
+    }
+
+    public function includeCategory(Topic $topic)
+    {
+        return $this->item($topic->category, new CategoryTransformer());
     }
 }
