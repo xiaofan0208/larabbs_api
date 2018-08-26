16bb9de9641a5d5736f2328f825ccc8d09673a97
xiaofan0208 <xiaofan0208@126.com>
2018-08-26 06:59:35 +0000

    include user roles

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -11,6 +11,8 @@ use League\Fractal\TransformerAbstract;
  */
 class UserTransformer extends TransformerAbstract
 {
+    protected $availableIncludes = ['roles'];
+
     public function transform(User $user)
     {
         return [
@@ -25,5 +27,10 @@ class UserTransformer extends TransformerAbstract
             'created_at' => $user->created_at->toDateTimeString(),
             'updated_at' => $user->updated_at->toDateTimeString(),
         ];
+    }
+
+    public function includeRoles(User $user)
+    {
+        return $this->collection($user->roles , new RoleTransformer() );
     }
 }
