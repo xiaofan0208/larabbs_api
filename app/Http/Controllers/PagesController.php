988bca3a80d013b3d27b89d22d9d865bbda53d63
xiaofan0208 <xiaofan0208@126.com>
2018-12-06 23:07:34 +0800

    修改后台条件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -18,7 +18,7 @@ class PagesController extends Controller
     public function permissionDenied()
     {
         // 如果当前用户有权限访问后台，直接跳转访问
-        if( config('administrator.permission') ){
+        if( config('administrator.permission')() ){
             return redirect( url( config('administrator.uri') ) , 302 );
         }
         // 否则使用视图
